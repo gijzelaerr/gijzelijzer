f78ce7edc62607325f7b6d35d10805ca44b4d4fc
Gijs Molenaar <gijs@pythonic.nl>
2011-09-20 13:58:46 +0200

    cleanup project

--- src/gijzelijzer.cpp
+++ src/gijzelijzer.cpp
@@ -1,6 +1,367 @@
+
 #include "gijzelijzer.h"
+#include "gmnames.h"
 
 
-Gijzelijzer::Gijzelijzer()
+AudioEffect* createEffectInstance (audioMasterCallback audioMaster)
 {
+        return new Gijzelijzer (audioMaster);
+}
+
+
+GijzelijzerProgram::GijzelijzerProgram ()
+{
+    // Default Program Values
+    fWaveform1 = 0.f;    // saw
+    fFreq1     =.0f;
+    fVolume1   = .33f;
+
+    fWaveform2 = 1.f;    // pulse
+    fFreq2     = .05f;    // slightly higher
+    fVolume2   = .33f;
+
+    fVolume    = .9f;
+    vst_strncpy (name, "Basic", kVstMaxProgNameLen);
+}
+
+
+Gijzelijzer::Gijzelijzer (audioMasterCallback audioMaster)
+: AudioEffectX (audioMaster, kNumPrograms, kNumParams)
+{
+    // initialize programs
+        programs = new GijzelijzerProgram[kNumPrograms];
+    for (VstInt32 i = 0; i < 16; i++)
+        channelPrograms[i] = i;
+
+    if (programs)
+        setProgram (0);
+
+    if (audioMaster)
+    {
+        setNumInputs (0);    	    	// no inputs
+        setNumOutputs (kNumOutputs);    // 2 outputs, 1 for each oscillator
+        canProcessReplacing ();
+        isSynth ();
+                setUniqueID ('5864');
+    }
+
+    initProcess ();
+    suspend ();
+}
+
+
+Gijzelijzer::~Gijzelijzer ()
+{
+    if (programs)
+        delete[] programs;
+}
+
+
+void Gijzelijzer::setProgram (VstInt32 program)
+{
+    if (program < 0 || program >= kNumPrograms)
+        return;
+
+        GijzelijzerProgram *ap = &programs[program];
+    curProgram = program;
+
+    fWaveform1 = ap->fWaveform1;
+    fFreq1     = ap->fFreq1;
+    fVolume1   = ap->fVolume1;
+
+    fWaveform2 = ap->fWaveform2;
+    fFreq2     = ap->fFreq2;
+    fVolume2   = ap->fVolume2;
+
+    fVolume    = ap->fVolume;
+}
+
+
+void Gijzelijzer::setProgramName (char* name)
+{
+    vst_strncpy (programs[curProgram].name, name, kVstMaxProgNameLen);
+}
+
+
+void Gijzelijzer::getProgramName (char* name)
+{
+    vst_strncpy (name, programs[curProgram].name, kVstMaxProgNameLen);
+}
+
+
+void Gijzelijzer::getParameterLabel (VstInt32 index, char* label)
+{
+    switch (index)
+    {
+        case kWaveform1:
+        case kWaveform2:
+            vst_strncpy (label, "Shape", kVstMaxParamStrLen);
+            break;
+
+        case kFreq1:
+        case kFreq2:
+            vst_strncpy (label, "Hz", kVstMaxParamStrLen);
+            break;
+
+        case kVolume1:
+        case kVolume2:
+        case kVolume:
+            vst_strncpy (label, "dB", kVstMaxParamStrLen);
+            break;
+    }
+}
+
+
+void Gijzelijzer::getParameterDisplay (VstInt32 index, char* text)
+{
+    text[0] = 0;
+    switch (index)
+    {
+        case kWaveform1:
+            if (fWaveform1 < .5)
+                vst_strncpy (text, "Sawtooth", kVstMaxParamStrLen);
+            else
+                vst_strncpy (text, "Pulse", kVstMaxParamStrLen);
+            break;
+
+        case kFreq1:    	float2string (fFreq1, text, kVstMaxParamStrLen);    break;
+        case kVolume1:    	dB2string (fVolume1, text, kVstMaxParamStrLen);    break;
+
+        case kWaveform2:
+            if (fWaveform2 < .5)
+                vst_strncpy (text, "Sawtooth", kVstMaxParamStrLen);
+            else
+                vst_strncpy (text, "Pulse", kVstMaxParamStrLen);
+            break;
+
+        case kFreq2:    	float2string (fFreq2, text, kVstMaxParamStrLen);    break;
+        case kVolume2:    	dB2string (fVolume2, text, kVstMaxParamStrLen);    break;
+        case kVolume:    	dB2string (fVolume, text, kVstMaxParamStrLen);    break;
+    }
+}
+
+
+void Gijzelijzer::getParameterName (VstInt32 index, char* label)
+{
+    switch (index)
+    {
+        case kWaveform1:    vst_strncpy (label, "Wave 1", kVstMaxParamStrLen);    break;
+        case kFreq1:    	vst_strncpy (label, "Freq 1", kVstMaxParamStrLen);    break;
+        case kVolume1:    	vst_strncpy (label, "Levl 1", kVstMaxParamStrLen);    break;
+        case kWaveform2:    vst_strncpy (label, "Wave 2", kVstMaxParamStrLen);    break;
+        case kFreq2:    	vst_strncpy (label, "Freq 2", kVstMaxParamStrLen);    break;
+        case kVolume2:    	vst_strncpy (label, "Levl 2", kVstMaxParamStrLen);    break;
+        case kVolume:    	vst_strncpy (label, "Volume", kVstMaxParamStrLen);    break;
+    }
+}
+
+
+void Gijzelijzer::setParameter (VstInt32 index, float value)
+{
+        GijzelijzerProgram *ap = &programs[curProgram];
+    switch (index)
+    {
+        case kWaveform1:    fWaveform1    = ap->fWaveform1    = value;    break;
+        case kFreq1:    	fFreq1     	= ap->fFreq1    	= value;    break;
+        case kVolume1:    	fVolume1    = ap->fVolume1    	= value;    break;
+        case kWaveform2:    fWaveform2    = ap->fWaveform2    = value;    break;
+        case kFreq2:    	fFreq2    	= ap->fFreq2    	= value;    break;
+        case kVolume2:    	fVolume2    = ap->fVolume2    	= value;    break;
+        case kVolume:    	fVolume    	= ap->fVolume    	= value;    break;
+    }
+}
+
+
+float Gijzelijzer::getParameter (VstInt32 index)
+{
+    float value = 0;
+    switch (index)
+    {
+        case kWaveform1:    value = fWaveform1;    break;
+        case kFreq1:    	value = fFreq1;    	break;
+        case kVolume1:    	value = fVolume1;    break;
+        case kWaveform2:    value = fWaveform2;    break;
+        case kFreq2:    	value = fFreq2;    	break;
+        case kVolume2:    	value = fVolume2;    break;
+        case kVolume:    	value = fVolume;    break;
+    }
+    return value;
+}
+
+
+bool Gijzelijzer::getOutputProperties (VstInt32 index, VstPinProperties* properties)
+{
+    if (index < kNumOutputs)
+    {
+        vst_strncpy (properties->label, "Vstx ", 63);
+        char temp[11] = {0};
+        int2string (index + 1, temp, 10);
+        vst_strncat (properties->label, temp, 63);
+
+        properties->flags = kVstPinIsActive;
+        if (index < 2)
+            properties->flags |= kVstPinIsStereo;    // make channel 1+2 stereo
+        return true;
+    }
+    return false;
+}
+
+
+bool Gijzelijzer::getProgramNameIndexed (VstInt32 category, VstInt32 index, char* text)
+{
+    if (index < kNumPrograms)
+    {
+        vst_strncpy (text, programs[index].name, kVstMaxProgNameLen);
+        return true;
+    }
+    return false;
+}
+
+
+bool Gijzelijzer::getEffectName (char* name)
+{
+        vst_strncpy (name, "Gijzelijzer", kVstMaxEffectNameLen);
+    return true;
+}
+
+
+bool Gijzelijzer::getVendorString (char* text)
+{
+    vst_strncpy (text, "Steinberg Media Technologies", kVstMaxVendorStrLen);
+    return true;
+}
+
+
+bool Gijzelijzer::getProductString (char* text)
+{
+    vst_strncpy (text, "Vst Test Synth", kVstMaxProductStrLen);
+    return true;
+}
+
+
+VstInt32 Gijzelijzer::getVendorVersion ()
+{ 
+    return 1000;
+}
+
+
+VstInt32 Gijzelijzer::canDo (char* text)
+{
+    if (!strcmp (text, "receiveVstEvents"))
+        return 1;
+    if (!strcmp (text, "receiveVstMidiEvent"))
+        return 1;
+    if (!strcmp (text, "midiProgramNames"))
+        return 1;
+    return -1;    // explicitly can't do; 0 => don't know
+}
+
+
+VstInt32 Gijzelijzer::getNumMidiInputChannels ()
+{
+    return 1; // we are monophonic
+}
+
+
+VstInt32 Gijzelijzer::getNumMidiOutputChannels ()
+{
+    return 0; // no MIDI output back to Host app
+}
+
+
+// midi program names:
+// as an example, GM names are used here. in fact, Gijzelijzer doesn't even support
+// multi-timbral operation so it's really just for demonstration.
+// a 'real' instrument would have a number of voices which use the
+// programs[channelProgram[channel]] parameters when it receives
+// a note on message.
+VstInt32 Gijzelijzer::getMidiProgramName (VstInt32 channel, MidiProgramName* mpn)
+{
+    VstInt32 prg = mpn->thisProgramIndex;
+    if (prg < 0 || prg >= 128)
+        return 0;
+    fillProgram (channel, prg, mpn);
+    if (channel == 9)
+        return 1;
+    return 128L;
+}
+
+
+VstInt32 Gijzelijzer::getCurrentMidiProgram (VstInt32 channel, MidiProgramName* mpn)
+{
+    if (channel < 0 || channel >= 16 || !mpn)
+        return -1;
+    VstInt32 prg = channelPrograms[channel];
+    mpn->thisProgramIndex = prg;
+    fillProgram (channel, prg, mpn);
+    return prg;
+}
+
+
+void Gijzelijzer::fillProgram (VstInt32 channel, VstInt32 prg, MidiProgramName* mpn)
+{
+    mpn->midiBankMsb =
+    mpn->midiBankLsb = -1;
+    mpn->reserved = 0;
+    mpn->flags = 0;
+
+    if (channel == 9)    // drums
+    {
+        vst_strncpy (mpn->name, "Standard", 63);
+        mpn->midiProgram = 0;
+        mpn->parentCategoryIndex = 0;
+    }
+    else
+    {
+        vst_strncpy (mpn->name, GmNames[prg], 63);
+        mpn->midiProgram = (char)prg;
+        mpn->parentCategoryIndex = -1;    // for now
+
+        for (VstInt32 i = 0; i < kNumGmCategories; i++)
+        {
+            if (prg >= GmCategoriesFirstIndices[i] && prg < GmCategoriesFirstIndices[i + 1])
+            {
+                mpn->parentCategoryIndex = i;
+                break;
+            }
+        }
+    }
+}
+
+
+VstInt32 Gijzelijzer::getMidiProgramCategory (VstInt32 channel, MidiProgramCategory* cat)
+{
+    cat->parentCategoryIndex = -1;    // -1:no parent category
+    cat->flags = 0;    	    	    // reserved, none defined yet, zero.
+    VstInt32 category = cat->thisCategoryIndex;
+    if (channel == 9)
+    {
+        vst_strncpy (cat->name, "Drums", 63);
+        return 1;
+    }
+    if (category >= 0 && category < kNumGmCategories)
+        vst_strncpy (cat->name, GmCategories[category], 63);
+    else
+        cat->name[0] = 0;
+    return kNumGmCategories;
+}
+
+
+bool Gijzelijzer::hasMidiProgramsChanged (VstInt32 channel)
+{
+    return false;    // updateDisplay ()
+}
+
+
+bool Gijzelijzer::getMidiKeyName (VstInt32 channel, MidiKeyName* key)
+                                // struct will be filled with information for 'thisProgramIndex' and 'thisKeyNumber'
+                                // if keyName is "" the standard name of the key will be displayed.
+                                // if false is returned, no MidiKeyNames defined for 'thisProgramIndex'.
+{
+    // key->thisProgramIndex;    	// >= 0. fill struct for this program index.
+    // key->thisKeyNumber;    	    // 0 - 127. fill struct for this key number.
+    key->keyName[0] = 0;
+    key->reserved = 0;    	    	// zero
+    key->flags = 0;    	    	    // reserved, none defined yet, zero.
+    return false;
 }
--- test/src/vstapi.cpp
+++ test/src/vstapi.cpp
@@ -1,6 +1,6 @@
 //-------------------------------------------------------------------------------------------------------
 // VST Plug-Ins SDK
-// Version 2.4		$Date: 2006/11/13 09:08:28 $
+// Version 2.4    	$Date: 2006/11/13 09:08:28 $
 //
 // Category     : VST 2.x SDK Samples
 // Filename     : minihost.cpp
@@ -35,59 +35,59 @@ static VstIntPtr VSTCALLBACK HostCallback (AEffect* effect, VstInt32 opcode, Vst
 struct PluginLoader
 {
 //-------------------------------------------------------------------------------------------------------
-	void* module;
+    void* module;
 
-	PluginLoader ()
-	: module (0)
-	{}
+    PluginLoader ()
+    : module (0)
+    {}
 
-	~PluginLoader ()
-	{
-		if (module)
-		{
-		#if _WIN32
-			FreeLibrary ((HMODULE)module);
-		#elif TARGET_API_MAC_CARBON
-			CFBundleUnloadExecutable ((CFBundleRef)module);
-			CFRelease ((CFBundleRef)module);
-		#endif
-		}
-	}
+    ~PluginLoader ()
+    {
+    	if (module)
+    	{
+    	#if _WIN32
+    	    FreeLibrary ((HMODULE)module);
+    	#elif TARGET_API_MAC_CARBON
+    	    CFBundleUnloadExecutable ((CFBundleRef)module);
+    	    CFRelease ((CFBundleRef)module);
+    	#endif
+    	}
+    }
 
-	bool loadLibrary (const char* fileName)
-	{
-	#if _WIN32
-		module = LoadLibrary (fileName);
-	#elif TARGET_API_MAC_CARBON
-		CFStringRef fileNameString = CFStringCreateWithCString (NULL, fileName, kCFStringEncodingUTF8);
-		if (fileNameString == 0)
-			return false;
-		CFURLRef url = CFURLCreateWithFileSystemPath (NULL, fileNameString, kCFURLPOSIXPathStyle, false);
-		CFRelease (fileNameString);
-		if (url == 0)
-			return false;
-		module = CFBundleCreate (NULL, url);
-		CFRelease (url);
-		if (module && CFBundleLoadExecutable ((CFBundleRef)module) == false)
-			return false;
-	#endif
-		return module != 0;
-	}
+    bool loadLibrary (const char* fileName)
+    {
+    #if _WIN32
+    	module = LoadLibrary (fileName);
+    #elif TARGET_API_MAC_CARBON
+    	CFStringRef fileNameString = CFStringCreateWithCString (NULL, fileName, kCFStringEncodingUTF8);
+    	if (fileNameString == 0)
+    	    return false;
+    	CFURLRef url = CFURLCreateWithFileSystemPath (NULL, fileNameString, kCFURLPOSIXPathStyle, false);
+    	CFRelease (fileNameString);
+    	if (url == 0)
+    	    return false;
+    	module = CFBundleCreate (NULL, url);
+    	CFRelease (url);
+    	if (module && CFBundleLoadExecutable ((CFBundleRef)module) == false)
+    	    return false;
+    #endif
+    	return module != 0;
+    }
 
-	PluginEntryProc getMainEntry ()
-	{
-		PluginEntryProc mainProc = 0;
-	#if _WIN32
-		mainProc = (PluginEntryProc)GetProcAddress ((HMODULE)module, "VSTPluginMain");
-		if (!mainProc)
-			mainProc = (PluginEntryProc)GetProcAddress ((HMODULE)module, "main");
-	#elif TARGET_API_MAC_CARBON
-		mainProc = (PluginEntryProc)CFBundleGetFunctionPointerForName ((CFBundleRef)module, CFSTR("VSTPluginMain"));
-		if (!mainProc)
-			mainProc = (PluginEntryProc)CFBundleGetFunctionPointerForName ((CFBundleRef)module, CFSTR("main_macho"));
-	#endif
-		return mainProc;
-	}
+    PluginEntryProc getMainEntry ()
+    {
+    	PluginEntryProc mainProc = 0;
+    #if _WIN32
+    	mainProc = (PluginEntryProc)GetProcAddress ((HMODULE)module, "VSTPluginMain");
+    	if (!mainProc)
+    	    mainProc = (PluginEntryProc)GetProcAddress ((HMODULE)module, "main");
+    #elif TARGET_API_MAC_CARBON
+    	mainProc = (PluginEntryProc)CFBundleGetFunctionPointerForName ((CFBundleRef)module, CFSTR("VSTPluginMain"));
+    	if (!mainProc)
+    	    mainProc = (PluginEntryProc)CFBundleGetFunctionPointerForName ((CFBundleRef)module, CFSTR("main_macho"));
+    #endif
+    	return mainProc;
+    }
 //-------------------------------------------------------------------------------------------------------
 };
 
@@ -95,20 +95,20 @@ struct PluginLoader
 static bool checkPlatform ()
 {
 #if VST_64BIT_PLATFORM
-	printf ("*** This is a 64 Bit Build! ***\n");
+    printf ("*** This is a 64 Bit Build! ***\n");
 #else
-	printf ("*** This is a 32 Bit Build! ***\n");
+    printf ("*** This is a 32 Bit Build! ***\n");
 #endif
 
-	int sizeOfVstIntPtr = sizeof (VstIntPtr);
-	int sizeOfVstInt32 = sizeof (VstInt32);
-	int sizeOfPointer = sizeof (void*);
-	int sizeOfAEffect = sizeof (AEffect);
-	
-	printf ("VstIntPtr = %d Bytes, VstInt32 = %d Bytes, Pointer = %d Bytes, AEffect = %d Bytes\n\n",
-			sizeOfVstIntPtr, sizeOfVstInt32, sizeOfPointer, sizeOfAEffect);
+    int sizeOfVstIntPtr = sizeof (VstIntPtr);
+    int sizeOfVstInt32 = sizeof (VstInt32);
+    int sizeOfPointer = sizeof (void*);
+    int sizeOfAEffect = sizeof (AEffect);
+    
+    printf ("VstIntPtr = %d Bytes, VstInt32 = %d Bytes, Pointer = %d Bytes, AEffect = %d Bytes\n\n",
+    	    sizeOfVstIntPtr, sizeOfVstInt32, sizeOfPointer, sizeOfAEffect);
 
-	return sizeOfVstIntPtr == sizeOfPointer;
+    return sizeOfVstIntPtr == sizeOfPointer;
 }
 
 //-------------------------------------------------------------------------------------------------------
@@ -119,211 +119,211 @@ extern bool checkEffectEditor (AEffect* effect); // minieditor.cpp
 //-------------------------------------------------------------------------------------------------------
 int main (int argc, char* argv[])
 {
-	if (!checkPlatform ())
-	{
-		printf ("Platform verification failed! Please check your Compiler Settings!\n");
-		return -1;
-	}
+    if (!checkPlatform ())
+    {
+    	printf ("Platform verification failed! Please check your Compiler Settings!\n");
+    	return -1;
+    }
 
         const char* fileName = "gijzelijzer.dll";
 
-	if (argc > 1)
-		fileName = argv[1];
+    if (argc > 1)
+    	fileName = argv[1];
 
-	printf ("HOST> Load library...\n");
-	PluginLoader loader;
-	if (!loader.loadLibrary (fileName))
-	{
-		printf ("Failed to load VST Plugin library!\n");
-		return -1;
-	}
+    printf ("HOST> Load library...\n");
+    PluginLoader loader;
+    if (!loader.loadLibrary (fileName))
+    {
+    	printf ("Failed to load VST Plugin library!\n");
+    	return -1;
+    }
 
-	PluginEntryProc mainEntry = loader.getMainEntry ();
-	if (!mainEntry)
-	{
-		printf ("VST Plugin main entry not found!\n");
-		return -1;
-	}
+    PluginEntryProc mainEntry = loader.getMainEntry ();
+    if (!mainEntry)
+    {
+    	printf ("VST Plugin main entry not found!\n");
+    	return -1;
+    }
 
-	printf ("HOST> Create effect...\n");
-	AEffect* effect = mainEntry (HostCallback);
-	if (!effect)
-	{
-		printf ("Failed to create effect instance!\n");
-		return -1;
-	}
+    printf ("HOST> Create effect...\n");
+    AEffect* effect = mainEntry (HostCallback);
+    if (!effect)
+    {
+    	printf ("Failed to create effect instance!\n");
+    	return -1;
+    }
 
-	printf ("HOST> Init sequence...\n");
-	effect->dispatcher (effect, effOpen, 0, 0, 0, 0);
-	effect->dispatcher (effect, effSetSampleRate, 0, 0, 0, kSampleRate);
-	effect->dispatcher (effect, effSetBlockSize, 0, kBlockSize, 0, 0);
+    printf ("HOST> Init sequence...\n");
+    effect->dispatcher (effect, effOpen, 0, 0, 0, 0);
+    effect->dispatcher (effect, effSetSampleRate, 0, 0, 0, kSampleRate);
+    effect->dispatcher (effect, effSetBlockSize, 0, kBlockSize, 0, 0);
 
-	checkEffectProperties (effect);
-	checkEffectProcessing (effect);
-	checkEffectEditor (effect);
+    checkEffectProperties (effect);
+    checkEffectProcessing (effect);
+    checkEffectEditor (effect);
 
-	printf ("HOST> Close effect...\n");
-	effect->dispatcher (effect, effClose, 0, 0, 0, 0);
-	return 0;
+    printf ("HOST> Close effect...\n");
+    effect->dispatcher (effect, effClose, 0, 0, 0, 0);
+    return 0;
 }
 
 //-------------------------------------------------------------------------------------------------------
 void checkEffectProperties (AEffect* effect)
 {
-	printf ("HOST> Gathering properties...\n");
+    printf ("HOST> Gathering properties...\n");
 
-	char effectName[256] = {0};
-	char vendorString[256] = {0};
-	char productString[256] = {0};
+    char effectName[256] = {0};
+    char vendorString[256] = {0};
+    char productString[256] = {0};
 
-	effect->dispatcher (effect, effGetEffectName, 0, 0, effectName, 0);
-	effect->dispatcher (effect, effGetVendorString, 0, 0, vendorString, 0);
-	effect->dispatcher (effect, effGetProductString, 0, 0, productString, 0);
+    effect->dispatcher (effect, effGetEffectName, 0, 0, effectName, 0);
+    effect->dispatcher (effect, effGetVendorString, 0, 0, vendorString, 0);
+    effect->dispatcher (effect, effGetProductString, 0, 0, productString, 0);
 
-	printf ("Name = %s\nVendor = %s\nProduct = %s\n\n", effectName, vendorString, productString);
+    printf ("Name = %s\nVendor = %s\nProduct = %s\n\n", effectName, vendorString, productString);
 
-	printf ("numPrograms = %d\nnumParams = %d\nnumInputs = %d\nnumOutputs = %d\n\n", 
-			effect->numPrograms, effect->numParams, effect->numInputs, effect->numOutputs);
+    printf ("numPrograms = %d\nnumParams = %d\nnumInputs = %d\nnumOutputs = %d\n\n", 
+    	    effect->numPrograms, effect->numParams, effect->numInputs, effect->numOutputs);
 
-	// Iterate programs...
-	for (VstInt32 progIndex = 0; progIndex < effect->numPrograms; progIndex++)
-	{
-		char progName[256] = {0};
-		if (!effect->dispatcher (effect, effGetProgramNameIndexed, progIndex, 0, progName, 0))
-		{
-			effect->dispatcher (effect, effSetProgram, 0, progIndex, 0, 0); // Note: old program not restored here!
-			effect->dispatcher (effect, effGetProgramName, 0, 0, progName, 0);
-		}
-		printf ("Program %03d: %s\n", progIndex, progName);
-	}
+    // Iterate programs...
+    for (VstInt32 progIndex = 0; progIndex < effect->numPrograms; progIndex++)
+    {
+    	char progName[256] = {0};
+    	if (!effect->dispatcher (effect, effGetProgramNameIndexed, progIndex, 0, progName, 0))
+    	{
+    	    effect->dispatcher (effect, effSetProgram, 0, progIndex, 0, 0); // Note: old program not restored here!
+    	    effect->dispatcher (effect, effGetProgramName, 0, 0, progName, 0);
+    	}
+    	printf ("Program %03d: %s\n", progIndex, progName);
+    }
 
-	printf ("\n");
+    printf ("\n");
 
-	// Iterate parameters...
-	for (VstInt32 paramIndex = 0; paramIndex < effect->numParams; paramIndex++)
-	{
-		char paramName[256] = {0};
-		char paramLabel[256] = {0};
-		char paramDisplay[256] = {0};
+    // Iterate parameters...
+    for (VstInt32 paramIndex = 0; paramIndex < effect->numParams; paramIndex++)
+    {
+    	char paramName[256] = {0};
+    	char paramLabel[256] = {0};
+    	char paramDisplay[256] = {0};
 
-		effect->dispatcher (effect, effGetParamName, paramIndex, 0, paramName, 0);
-		effect->dispatcher (effect, effGetParamLabel, paramIndex, 0, paramLabel, 0);
-		effect->dispatcher (effect, effGetParamDisplay, paramIndex, 0, paramDisplay, 0);
-		float value = effect->getParameter (effect, paramIndex);
+    	effect->dispatcher (effect, effGetParamName, paramIndex, 0, paramName, 0);
+    	effect->dispatcher (effect, effGetParamLabel, paramIndex, 0, paramLabel, 0);
+    	effect->dispatcher (effect, effGetParamDisplay, paramIndex, 0, paramDisplay, 0);
+    	float value = effect->getParameter (effect, paramIndex);
 
-		printf ("Param %03d: %s [%s %s] (normalized = %f)\n", paramIndex, paramName, paramDisplay, paramLabel, value);
-	}
+    	printf ("Param %03d: %s [%s %s] (normalized = %f)\n", paramIndex, paramName, paramDisplay, paramLabel, value);
+    }
 
-	printf ("\n");
+    printf ("\n");
 
-	// Can-do nonsense...
-	static const char* canDos[] =
-	{
-		"receiveVstEvents",
-		"receiveVstMidiEvent",
-		"midiProgramNames"
-	};
+    // Can-do nonsense...
+    static const char* canDos[] =
+    {
+    	"receiveVstEvents",
+    	"receiveVstMidiEvent",
+    	"midiProgramNames"
+    };
 
-	for (VstInt32 canDoIndex = 0; canDoIndex < sizeof (canDos) / sizeof (canDos[0]); canDoIndex++)
-	{
-		printf ("Can do %s... ", canDos[canDoIndex]);
-		VstInt32 result = (VstInt32)effect->dispatcher (effect, effCanDo, 0, 0, (void*)canDos[canDoIndex], 0);
-		switch (result)
-		{
-			case 0  : printf ("don't know"); break;
-			case 1  : printf ("yes"); break;
-			case -1 : printf ("definitely not!"); break;
-			default : printf ("?????");
-		}
-		printf ("\n");
-	}
+    for (VstInt32 canDoIndex = 0; canDoIndex < sizeof (canDos) / sizeof (canDos[0]); canDoIndex++)
+    {
+    	printf ("Can do %s... ", canDos[canDoIndex]);
+    	VstInt32 result = (VstInt32)effect->dispatcher (effect, effCanDo, 0, 0, (void*)canDos[canDoIndex], 0);
+    	switch (result)
+    	{
+    	    case 0  : printf ("don't know"); break;
+    	    case 1  : printf ("yes"); break;
+    	    case -1 : printf ("definitely not!"); break;
+    	    default : printf ("?????");
+    	}
+    	printf ("\n");
+    }
 
-	printf ("\n");
+    printf ("\n");
 }
 
 //-------------------------------------------------------------------------------------------------------
 void checkEffectProcessing (AEffect* effect)
 {
-	float** inputs = 0;
-	float** outputs = 0;
-	VstInt32 numInputs = effect->numInputs;
-	VstInt32 numOutputs = effect->numOutputs;
-	
-	if (numInputs > 0)
-	{
-		inputs = new float*[numInputs];
-		for (VstInt32 i = 0; i < numInputs; i++)
-		{
-			inputs[i] = new float[kBlockSize];
-			memset (inputs[i], 0, kBlockSize * sizeof (float));
-		}
-	}
+    float** inputs = 0;
+    float** outputs = 0;
+    VstInt32 numInputs = effect->numInputs;
+    VstInt32 numOutputs = effect->numOutputs;
+    
+    if (numInputs > 0)
+    {
+    	inputs = new float*[numInputs];
+    	for (VstInt32 i = 0; i < numInputs; i++)
+    	{
+    	    inputs[i] = new float[kBlockSize];
+    	    memset (inputs[i], 0, kBlockSize * sizeof (float));
+    	}
+    }
 
-	if (numOutputs > 0)
-	{
-		outputs = new float*[numOutputs];
-		for (VstInt32 i = 0; i < numOutputs; i++)
-		{
-			outputs[i] = new float[kBlockSize];
-			memset (outputs[i], 0, kBlockSize * sizeof (float));
-		}
-	}
+    if (numOutputs > 0)
+    {
+    	outputs = new float*[numOutputs];
+    	for (VstInt32 i = 0; i < numOutputs; i++)
+    	{
+    	    outputs[i] = new float[kBlockSize];
+    	    memset (outputs[i], 0, kBlockSize * sizeof (float));
+    	}
+    }
 
-	printf ("HOST> Resume effect...\n");
-	effect->dispatcher (effect, effMainsChanged, 0, 1, 0, 0);
+    printf ("HOST> Resume effect...\n");
+    effect->dispatcher (effect, effMainsChanged, 0, 1, 0, 0);
 
-	for (VstInt32 processCount = 0; processCount < kNumProcessCycles; processCount++)
-	{
-		printf ("HOST> Process Replacing...\n");
-		effect->processReplacing (effect, inputs, outputs, kBlockSize);
-	}
+    for (VstInt32 processCount = 0; processCount < kNumProcessCycles; processCount++)
+    {
+    	printf ("HOST> Process Replacing...\n");
+    	effect->processReplacing (effect, inputs, outputs, kBlockSize);
+    }
 
-	printf ("HOST> Suspend effect...\n");
-	effect->dispatcher (effect, effMainsChanged, 0, 0, 0, 0);
+    printf ("HOST> Suspend effect...\n");
+    effect->dispatcher (effect, effMainsChanged, 0, 0, 0, 0);
 
-	if (numInputs > 0)
-	{
-		for (VstInt32 i = 0; i < numInputs; i++)
-			delete [] inputs[i];
-		delete [] inputs;
-	}
+    if (numInputs > 0)
+    {
+    	for (VstInt32 i = 0; i < numInputs; i++)
+    	    delete [] inputs[i];
+    	delete [] inputs;
+    }
 
-	if (numOutputs > 0)
-	{
-		for (VstInt32 i = 0; i < numOutputs; i++)
-			delete [] outputs[i];
-		delete [] outputs;
-	}
+    if (numOutputs > 0)
+    {
+    	for (VstInt32 i = 0; i < numOutputs; i++)
+    	    delete [] outputs[i];
+    	delete [] outputs;
+    }
 }
 
 //-------------------------------------------------------------------------------------------------------
 VstIntPtr VSTCALLBACK HostCallback (AEffect* effect, VstInt32 opcode, VstInt32 index, VstIntPtr value, void* ptr, float opt)
 {
-	VstIntPtr result = 0;
+    VstIntPtr result = 0;
 
-	// Filter idle calls...
-	bool filtered = false;
-	if (opcode == audioMasterIdle)
-	{
-		static bool wasIdle = false;
-		if (wasIdle)
-			filtered = true;
-		else
-		{
-			printf ("(Future idle calls will not be displayed!)\n");
-			wasIdle = true;
-		}
-	}
+    // Filter idle calls...
+    bool filtered = false;
+    if (opcode == audioMasterIdle)
+    {
+    	static bool wasIdle = false;
+    	if (wasIdle)
+    	    filtered = true;
+    	else
+    	{
+    	    printf ("(Future idle calls will not be displayed!)\n");
+    	    wasIdle = true;
+    	}
+    }
 
-	if (!filtered)
-		printf ("PLUG> HostCallback (opcode %d)\n index = %d, value = %p, ptr = %p, opt = %f\n", opcode, index, FromVstPtr<void> (value), ptr, opt);
+    if (!filtered)
+    	printf ("PLUG> HostCallback (opcode %d)\n index = %d, value = %p, ptr = %p, opt = %f\n", opcode, index, FromVstPtr<void> (value), ptr, opt);
 
-	switch (opcode)
-	{
-		case audioMasterVersion :
-			result = kVstVersion;
-			break;
-	}
+    switch (opcode)
+    {
+    	case audioMasterVersion :
+    	    result = kVstVersion;
+    	    break;
+    }
 
-	return result;
+    return result;
 }
